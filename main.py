db5d17e53933556f77986dc4b0ea96c9d32f0369
1Pampu <piampianimartin@gmail.com>
2023-07-15 17:19:08 -0300

    update

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 # Import Libraries
 import json
-import random
 from fastapi import FastAPI,HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-
-#  Import our pokemon database into a list
-# dbRoute = "DataBase/pokemon-used.json"
-dbRoute = "/opt/render/project/src/DataBase/pokemon-used.json"      # Change the path of where you contain the file
-with open(dbRoute,'r') as file:
-    contentJson = json.load(file)
-pokemonList = list(contentJson)
+from pydantic import BaseModel
+import random
+import asyncio
 
 # Create a new instance of the FastAPI application
 app = FastAPI()
@@ -23,6 +18,32 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+# Import our pokemon database into a list
+dbRoute = "DataBase/pokemon-used.json"
+#dbRoute = "/opt/render/project/src/DataBase/pokemon-used.json"      # Change the path of where you contain the file
+with open(dbRoute,'r') as file:
+    contentJson = json.load(file)
+pokemonList = list(contentJson)
+
+# Create a pokemon class
+class Pokemon(BaseModel):
+    name: str
+
+# Variable that contains the pokemon of the "day"
+pkmonday = None
+
+# Function that changes the pokemon of the day every X time
+async def periodicChange():
+    global pkmonDay   # Specify that we are reffering to the global variable
+    while True:
+        pkmonDay = random.choice(pokemonList)
+        await asyncio.sleep(900)   # 900 = 15min
+
+# Start async function at startup
+@app.on_event("startup")
+async def startTasks():
+    asyncio.create_task(periodicChange())
 
 # Returns all the pokemon list
 @app.get("/")
@@ -57,6 +78,16 @@ async def searchByOthers(name: str = None, type1: str = None, type2: str = None,
     return pokemonToReturn
 
 # Return random pokemon when requested
-@app.get("/random")
+@app.get("/random/")
 async def pokeRandom():
     return random.choice(pokemonList)
+
+# Validate the recived pokemon
+@app.post("/answer")
+async def validateAnswer(guess: Pokemon):
+    if guess.name.lower() == pkmonDay["name"].lower():
+        return {"message": "correct answer!"}
+    elif any(pokemon["name"].lower() == guess.name.lower() for pokemon in pokemonList):
+        return {"message": "incorrect answer"}
+    else:
+        return {"message": "That pokemon doesn't exist"}
